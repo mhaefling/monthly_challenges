b0281ffce5cc2bdc360892bbc82653e1bbd09b03
Matt Haefling <m.haefling@protonmail.com>
2026-03-30 16:36:37 -0600

    additional paths, and urls

--- challenges/views.py
+++ challenges/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This works!")
+def january(request):
+    return HttpResponse("Eath no meat for the entire month!")
+
+def februrary(request):
+    return HttpResponse("Walk for at least 20 mins a day!")

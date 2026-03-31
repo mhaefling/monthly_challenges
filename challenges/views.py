b8d863d3456bc98ae862d578d472276c8223859d
Matt Haefling <m.haefling@protonmail.com>
2026-03-30 18:56:29 -0600

    dyamic path values

--- challenges/views.py
+++ challenges/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Eath no meat for the entire month!")
-
-def februrary(request):
-    return HttpResponse("Walk for at least 20 mins a day!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat no meat for the entire month!"
+    elif month == "february":
+        challenge_text = "Walk for at least 20 minutes a day!"
+    elif month == "march":
+        challenge_text = "Learn Django for at least 20 minutes every day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    
+    return HttpResponse(challenge_text)

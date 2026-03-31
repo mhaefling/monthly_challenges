35199566f1f613ebca19e2315b5fc834ca503a12
Matt Haefling <m.haefling@protonmail.com>
2026-03-31 08:51:55 -0600

    Redirects and Dynamic URLs

--- challenges/views.py
+++ challenges/views.py
@@ -1,29 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
 
-def monthly_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Eat no meat for the entire month!"
-    elif month == "february":
-        challenge_text = "Walk for at least 20 minutes a day!"
-    elif month == "march":
-        challenge_text = "Learn Django for at least 20 minutes every day!"
-    else:
-        return HttpResponseNotFound("This month is not supported!")
-    
-    return HttpResponse(challenge_text)
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes a day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Study for the CySA+ every day!",
+    "may": "Ride your bike every day!",
+    "june": "Its your birthday, take a break!",
+    "july": "Work out for 2 hours a day!",
+    "august": "Eat no meat for the entire month!",
+    "september": "Take a 2 hour nap every day!",
+    "october": "Prepare for Halloween!",
+    "november": "Prepare for Thanksgiving!",
+    "december": "Prepare for Christmas!"
+}
 
-def monthly_challenges_by_number(request, month):
-    month_text = None
-    if month == 1:
-        month_text = "January!"
-    elif month == 2:
-        month_text = "February!"
-    elif month == 3:
-        month_text = "March!"
-    else:
+def monthly_challenge(request, month):
+    try:
+        return HttpResponse(monthly_challenges[month])
+    except:
         return HttpResponseNotFound("Invalid Month!")
     
-    return HttpResponse(month_text)
+
+def monthly_challenges_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    try:
+        return HttpResponseRedirect("/challenges/" + months[month - 1])
+    except:
+        return HttpResponseNotFound("Invalid Month!")
+
